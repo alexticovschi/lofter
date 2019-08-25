f3c9d2b0be09ca0731403bf26060fa312a1b52c4
AlexTicovschi <alexmticovschi@gmail.com>
2019-08-25 19:04:58 +0100

    building the BestOf section

--- src/components/BestOf/BestOf.jsx
+++ src/components/BestOf/BestOf.jsx
@@ -1,11 +1,34 @@
 import React from "react";
+import chair from "../../Resources/img/chair.jpg";
+import lamp from "../../Resources/img/lamp.jpg";
+import table from "../../Resources/img/table.jpg";
+import chair2 from "../../Resources/img/chair2.jpg";
 
 import "./bestof.scss";
 
 const BestOf = () => {
   return (
     <section className="bestof__wrapper">
-      Best oF
+      <div className="bestof">
+        <img className="bestof__img" src={chair} alt="chair" />
+        <h4 className="heading-4 heading-4--dark">Best chair ever</h4>
+        <p className="bestof__text">$189</p>
+      </div>
+      <div className="bestof">
+        <img className="bestof__img" src={lamp} alt="lamp" />
+        <h4 className="heading-4 heading-4--dark">Best lamp ever</h4>
+        <p className="bestof__text">$189</p>
+      </div>
+      <div className="bestof">
+        <img className="bestof__img" src={table} alt="table" />
+        <h4 className="heading-4 heading-4--dark">Best table ever</h4>
+        <p className="bestof__text">$189</p>
+      </div>
+      <div className="bestof">
+        <img className="bestof__img" src={chair2} alt="table" />
+        <h4 className="heading-4 heading-4--dark">Best chair ever</h4>
+        <p className="bestof__text">$189</p>
+      </div>
     </section>
   );
 };
