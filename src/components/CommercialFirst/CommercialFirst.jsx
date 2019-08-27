d99cb018ad5e02b28b4768428a460fae6f0c6d74
AlexTicovschi <alexmticovschi@gmail.com>
2019-08-27 09:12:01 +0100

    building the Commercial sections

--- src/components/CommercialFirst/CommercialFirst.jsx
+++ src/components/CommercialFirst/CommercialFirst.jsx
@@ -1,9 +1,25 @@
 import React from "react";
+import commercial from "../../Resources/img/commercial.jpg";
 
 import "./commercialFirst.scss";
 
 const CommercialFirst = () => {
-  return <section className="commercial-first">Commercial First</section>;
+  return (
+    <section className="commercial-first">
+      <img
+        src={commercial}
+        alt="commercial"
+        className="commercial-first__img"
+      />
+      <div className="commercial__article-container article__container">
+        <h2 className="article__container--title">Accessible accessorising</h2>
+        <p className="article__container--content">
+          Colour lover and fashion cool girl, Francesca Antonini shares her tips
+          on making your interior choices wheelchair friendly.
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default CommercialFirst;
--- src/components/CommercialSecond/CommercialSecond.jsx
+++ src/components/CommercialSecond/CommercialSecond.jsx
@@ -1,11 +1,26 @@
 import React from "react";
+import commercial2 from "../../Resources/img/commercial2.jpg";
 
 import "./commercialSecond.scss";
 
 const CommercialSecond = () => {
   return (
     <section className="commercial-second">
-     Commercial second
+      <div className="commercial__article-container-second article__container-second">
+        <h2 className="article__container-second--title">
+          Interior design service
+        </h2>
+        <p className="article__container-second--content">
+          At LOFTER Interiors we have a dedicated interior design service
+          providing each client with a personalised interior design experience
+          from initial consultation through to completion.
+        </p>
+      </div>
+      <img
+        src={commercial2}
+        alt="commercial 2"
+        className="commercial-second__img"
+      />
     </section>
   );
 };
